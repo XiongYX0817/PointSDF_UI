29042b39745c889c5bf982903b9d552f42c6765c
XiongYX0817 <xiongyx@sjtu.edu.cn>
2023-02-27 13:13:44 +0800

    update

--- code/utils/plots.py
+++ code/utils/plots.py
@@ -196,7 +196,6 @@ def get_surface_high_res_mesh(sdf, resolution=100):
     s_cov = recon_pc - s_mean
     s_cov = torch.mm(s_cov.transpose(0, 1), s_cov)
     vecs = torch.linalg.eig(s_cov)[1].transpose(0, 1)
-    print(vecs)
     if torch.det(vecs) < 0:
         vecs = torch.mm(torch.tensor([[1, 0, 0], [0, 0, 1], [0, 1, 0]]).cuda().float(), vecs)
     helper = torch.bmm(vecs.unsqueeze(0).repeat(recon_pc.shape[0], 1, 1),
